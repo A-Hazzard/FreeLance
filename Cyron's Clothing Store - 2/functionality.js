f32ed26e679bbaf02e970b05f7c12b5e0dfcfc3f
Aaron Hazzard <aaronsploit@gmail.com>
2022-07-26 17:27:21 -0700

    Home animation

--- Cyron's Clothing Store - 2/functionality.js
+++ Cyron's Clothing Store - 2/functionality.js
@@ -17,3 +17,28 @@ function navigation() {
       }
     }
   }
+
+  $(document).ready(function() {
+
+    $(window).resize(function(){
+        $(window).scroll(function() {
+      
+          if ($(document).scrollTop() > 200) 
+          $("#welcome").addClass("newWelcome")
+
+          if ($(document).scrollTop() > 200)
+          $("#home-banner-img").addClass("animatedBannerImg")
+
+          if($(document).scrollTop() > 705)
+          $(".c1").addClass("animatedCard1")
+
+          if($(document).scrollTop() > 1000)
+          $(".c2").addClass("animatedCard2")
+          
+          if($(document).scrollTop() > 1500)
+          $(".c3").addClass("animatedCard3")
+        });
+      
+    });
+  
+  });
